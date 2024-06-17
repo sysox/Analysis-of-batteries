a27866ce367067aaead255dff2500b90ffbe0285
sysox <marek.sys@gmail.com>
2024-06-17 14:14:24 +0200

    GoF names fixed

--- src/python/names.py
+++ src/python/names.py
@@ -7,6 +7,11 @@ GoFs = ["dieharder_default", "dieharder_kuiper", "nist_chi2", "testu01_snpair_Cl
         "testu01_sknuth_MaxOft", "testu01_ksp", "testu01_ksm", "testu01_ks",
         "testu01_ad", "testu01_cm", "testu01_wg", "testu01_wu", "marsa_KS_left",
         "marsa_KS_right", "marsa_KS_both", "dieharder_corrected", "dieharder_fast"]
+GoF_test_names = {0:"dieharder_default", 1:"dieharder_kuiper", 2:"nist_chi2", 3:"testu01_snpair_ClosePairs",
+                  4:"testu01_sknuth_MaxOft", 5:"testu01_ksp", 6:"testu01_ksm", 7:"testu01_ks",
+                  8:"testu01_ad", 9:"testu01_cm", 10:"testu01_wg", 11:"testu01_wu",
+                  12:"marsa_KS_left", 13:"marsa_KS_right", 14:"marsa_KS", 15:"dieharder_corrected",
+                  16:"dieharder_fast"}
 
 files_names_examples = ["Dieharder(4) Diehard Bitstream Test.pval",
                         "Dieharder(208) DAB Fill Tree Test 2 Subtest 2.pval",
@@ -66,11 +71,7 @@ def extract_from_path(path):
 dll_path = '/mnt/c/Users/user/PycharmProjects/Analysis-of-batteries/src/C/libpvals.so'
 GoF_test_ids = list(range(17))
 
-GoF_test_names = {0:"dieharder_pvalue", 1:"dieharder_pvalue_kuiper", 2:"nist_pvalue", 3:"testu01_pvalue_snpair_ClosePairs",
-       4:"testu01_pvalue_sknuth_MaxOft", 5:"testu01_pvalue_ksp", 6:"testu01_pvalue_ksm", 7:"testu01_pvalue_ks",
-       8:"testu01_pvalue_ad", 9:"testu01_pvalue_cm", 10:"testu01_pvalue_wg", 11:"testu01_pvalue_wu",
-        12:"marsa_KS_left_pvalue", 13:"marsa_KS_right_pvalue", 14:"marsa_both_pvalue", 15:"dieharder_corrected_pvalue",
-        16:"dieharder_fast_pvalue"}
+
 
 # def testname_to_GoF_idx(test_name):
 #     if 'Diehard' in test_name:
